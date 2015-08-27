b51bddb8608b6d5750471304cb38b572eb1cb561
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-08-27 17:29:08 +0300

    Changed links to buttons, ng-click works better that way.
    Alaos changed styles. Removed outline from all the link-like buttons.

--- app/scripts/controllers/discussions.js
+++ app/scripts/controllers/discussions.js
@@ -8,10 +8,33 @@
  * Controller of the discussionToolApp
  */
 angular.module('discussionToolApp')
-  .controller('DiscussionsCtrl', function ($rootScope, $scope, $location, $routeParams, discussionsService, entitiesService) {
+  .controller('DiscussionsCtrl', function ($rootScope, $scope, $location, $routeParams, discussionsService, entitiesService, authService, config) {
+    function constructFileDownloadUri (uri) {
+      return config.sssRestUrl + 'files/files/download' +
+      '?file=' + encodeURIComponent(uri) +
+      '&key=' + encodeURIComponent(authService.getAuthKey());
+    }
+
     var targetUri = decodeURIComponent($routeParams.target);
 
     $rootScope.targetEntityUri = targetUri;
+
+    $scope.attachedEntityClicked = function (entity, event) {
+      angular.element(event.currentTarget).blur();
+
+      if ( entity.type === 'placeholder') {
+        return;
+      } else if ( entity.type === 'evernoteResource' || entity.type === 'evernoteNote' ) {
+        if ( entity.file ) {
+          window.open(constructFileDownloadUri(entity.file.id));
+        }
+        return;
+      } else if ( entity.type === 'file' ) {
+        window.open(constructFileDownloadUri(entity.id));
+        return;
+      }
+      window.open(entity.id);
+    };
 
     $scope.startNewDiscussion = function () {
       $location.path('discussions/' + encodeURIComponent(targetUri) + '/discussion/create');
@@ -34,7 +57,8 @@ angular.module('discussionToolApp')
       {
         setLikes: true,
         setTags: true,
-        setAttachedEntities: true
+        setAttachedEntities: true,
+        setEntries: true
       }, function (discussions) {
         $scope.discussions = discussions;
       });
