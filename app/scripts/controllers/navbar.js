544bd448067724d9efeb38037292a5e218e9004a
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-09-04 15:28:35 +0300

    Added BnP button to navbar.

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -8,11 +8,16 @@
  * Controller of the discussionToolApp
  */
 angular.module('discussionToolApp')
-  .controller('NavbarCtrl', function ($rootScope, $scope, authService) {
+  .controller('NavbarCtrl', function ($rootScope, $scope, config, authService) {
     $scope.logOut = function () {
       authService.removeAuthCookie();
     };
+
     $scope.getTargetEntityUri = function () {
         return encodeURIComponent(encodeURIComponent($rootScope.targetEntityUri));
+    };
+
+    $scope.navigateToBnp = function () {
+      window.open(config.bnpUrl);
     };
   });
--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -14,5 +14,5 @@ angular.module('discussionToolApp')
     oidcClientId: '03d7dd09-e99e-43ea-bd93-d06661426c95',
     sssRestUrl: 'http://localhost:8080/sss.adapter.rest.v2/',
     ldRestUrl: 'http://localhost:8080/living.documents/',
-    bnpUrl: 'http://localhost:8080/BitsAndPieces'
+    bnpUrl: 'http://localhost:8080/BitsAndPiece/'
   });
