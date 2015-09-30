b2459746d12b3d56db5f978bc074fc0d4a6486c5
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-09-30 12:30:01 +0300

    Cleared config before tagging a release.

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -10,10 +10,10 @@
 angular.module('discussionToolApp')
   .constant('config', {
     authCookieName: 'DiscussionToolAuth',
-    oidcAuthorizationUrl: 'https://api.learning-layers.eu/o/oauth2/authorize',
-    oidcClientId: '03d7dd09-e99e-43ea-bd93-d06661426c95',
-    sssRestUrl: 'http://test-ll.know-center.tugraz.at/layers.test/',
-    ldRestUrl: 'http://178.62.62.23:9000/',
-    ldClientUrl: 'http://178.62.62.23/',
-    bnpUrl: 'http://localhost/BitsAndPieces/'
+    oidcAuthorizationUrl: '',
+    oidcClientId: '',
+    sssRestUrl: '',
+    ldRestUrl: '',
+    ldClientUrl: '',
+    bnpUrl: ''
   });
